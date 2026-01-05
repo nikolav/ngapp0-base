80f1aa77fc8415e6e365c74ac14df346e684ad53
Vukovic Nikola <admin@nikolav.rs>
2026-01-05 04:15:58 +0100

    route-transitions blur-in-out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 
 import { CoreModulesShared } from "./modules";
+import { routeTransitionBlurInOut } from "./assets/route-transitions";
 
 @Component({
   selector: "app-root",
   imports: [CoreModulesShared],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
+  animations: [routeTransitionBlurInOut],
 })
 export class AppComponent {
   title = "app";
